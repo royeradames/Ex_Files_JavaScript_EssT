5d2dac3923c50a1d028aceed1b8f9be6d42bae9a
royeradames <royeraadames@gmail.com>
2019-12-25 19:10:02 -0500

    starting the listeners and functions of my text speed game

--- Exercise Files/08_02/script.js
+++ Exercise Files/08_02/script.js
@@ -3,21 +3,41 @@ const testArea = document.querySelector("#test-area");
 const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
-
+let timer = [0,0,0,0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 
 
 // Run a standard minute/second/hundredths timer:
+function runTime(){
+    let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
 
+    
+}
 
 // Match the text entered with the provided text on the page:
+function spellCheck(){
+    let textEntered = testArea.value;
+    console.log(textEntered);
+};
 
-
-// Start the timer:
-
+// Start the runTime:
+function start(){
+    let textEnterLegngth = testArea.value.length;
+    console.log(textEnterLegngth);
+    if (textEnterLegngth === 0) {
+        setInterval(runTime, 10);
+    }
+};
 
 // Reset everything:
-
+function reset(){
+    testArea.value = "";
+};
 
 // Event listeners for keyboard input and the reset button:
+testWrapper.addEventListener("keydown", start, false);
+testWrapper.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
