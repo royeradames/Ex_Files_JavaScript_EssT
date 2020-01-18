f5b5033fc1a949a7b196181569557cab229fa1d1
royeradames <royeraadames@gmail.com>
2020-01-18 16:52:22 -0500

    typing app done

--- Exercise Files/08_02/script.js
+++ Exercise Files/08_02/script.js
@@ -5,10 +5,11 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 let timer = [0,         0,                        0,              0];
 //          minutes, seconds, hundredths of seconds (centisecond), centisecond without clear
-
+let interval;
+let timerRunning = false;
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time){
-    if(time <= 10 ){
+    if(time <= 9 ){
         return `0${time}`;
     }else{
     return time
@@ -30,26 +31,43 @@ function runTime(){
 // Match the text entered with the provided text on the page:
 function spellCheck(){
     let textEntered = testArea.value;
-    console.log(textEntered);
+    let originTextMatch = originText.substring(0, textEntered.length);
+    
+    
+    if(textEntered == originText){
+        clearInterval(interval);
+        testWrapper.style.borderColor = "#429890";//challange completed
+        
+    }else{//current but not finish
+        if(textEntered == originTextMatch){
+            testWrapper.style.borderColor = "#65CCF3";//BLUE WHEN YOU ARE WRITING THE TEXT CORRECTLY BUT NOT FINISH YET
+        }else{
+            testWrapper.style.borderColor = "#E95D0F"; //ORANGE WHEN YOU MAKE A MISTAKEZ
+        }
+    }
 };
 
 // Start the runTime:
 function start(){
     let textEnterLegngth = testArea.value.length; //when can I use value.length and when can I just do .Length?
-    if (textEnterLegngth === 0) {
-        setInterval(runTime, 10);//research setInterval()
+    if (textEnterLegngth === 0 && !timerRunning) {
+        timerRunning = true;
+        interval = setInterval(runTime, 10);//research setInterval()
     }
 };
 
 // Reset everything:
 function reset(){//finish this buttom
-    setTimeout
+    clearInterval(interval);
+    timer = [0,0,0,0];
     theTimer.innerHTML = "00:00:00";
     testArea.value = "";
+    timerRunning = false;
+    interval = null;
 };
 
 // Event listeners for keyboard input and the reset button:
-testWrapper.addEventListener("keydown", start, false);
+testWrapper.addEventListener("keypress", start, false);
 testWrapper.addEventListener("keyup", spellCheck, false);
 resetButton.addEventListener("click", reset, false);
 
