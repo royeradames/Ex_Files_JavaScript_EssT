30683dbb2d7f518ed8ac448fcfc8bdaade8348bf
royeradames <royeraadames@gmail.com>
2020-01-29 00:02:46 -0500

    added random array of text
    but I am getting an error and working on understanding what is happening and fixing it.

--- Exercise Files/royer inprove typying game/script.js
+++ Exercise Files/royer inprove typying game/script.js
@@ -1,16 +1,22 @@
 const testWrapper = document.querySelector(".test-wrapper");
 const testArea = document.querySelector("#test-area");
-const originText = document.querySelector("#origin-text p").innerHTML;
+const testingTextSpot = document.querySelector("#origin-text p");
+// const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 const wpmValue = document.querySelector(".wpm .value");
+const wpsValue = document.querySelector(".wps .value");
 const errorsValue = document.querySelector(".errors .value")
+
 
 let timer = [0,0,0,0];
 let interval;
 let timerRunning = false;
 let errorCounter = 0;
-
+let arrayText = ["They are written to engender faith in Jesus as the Messiah and the incarnation of God, who came to teach, suffer and die for people's sins. Jesus was born circa 6 B.C. in Bethlehem. His mother, Mary, was a virgin who was betrothed to Joseph, a carpenter.",
+"A software engineer applies mathematical analysis and the principles of computer science in order to design and develop computer software. There are many types of software that a software engineer can develop, such as operating systems, computer games, middleware, business applications and network control systems.",
+"Software Developers often work for computer firms and manufacturers. Their main role is to create the foundations for operative systems on which Computer Programmers work. They design, write, and test code for new systems and software to ensure efficiency."];
+let randomOriginText = "";
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
     if (time <= 9) {
@@ -33,12 +39,17 @@ function runTimer() {
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
-    let originTextMatch = originText.substring(0,textEntered.length);
-
-    if (textEntered == originText) { //Finish speed test
+    // let originTextMatch = originText.substring(0,textEntered.length);
+    let originTextMatch = randomOriginText.substring(0, textEntered.value);
+    console.log(textEntered);
+    console.log(textEntered.lenght);
+    console.log(randomOriginText.substring(0, textEntered.value));
+    console.log(originTextMatch);
+    if (textEntered == randomOriginText) { //Finish speed test
         clearInterval(interval);
         testWrapper.style.borderColor = "#429890";
-        wpmValue.innerHTML = wordsPerMinute(originText, timer[0]);
+        wpmValue.innerHTML = wordsPerTime(randomOriginText, timer[0]);
+        wpsValue.innerHTML = wordsPerTime(randomOriginText, timer[1]);
         //display the numbers of errors
         errorsValue.innerHTML = errorCounter;
     } else {
@@ -58,9 +69,10 @@ function start() {
     let textEnterdLength = testArea.value.length;
     if (textEnterdLength === 0 && !timerRunning) {
         timerRunning = true;
+        randomOriginText = testingTextSpot.innerHTML = arrayText[getRandomIndex()]; //populate origin text with new text from array
         interval = setInterval(runTimer, 10);
     }
-    console.log(textEnterdLength);
+    
 }
 
 // Reset everything:
@@ -85,16 +97,25 @@ resetButton.addEventListener("click", reset, false);
 
 //add a words per minute count
 // You can find the calculation for it online, and you can just grab the time that you calculated, and combine it with the number of words in your test, and then you can figure out exactly how fast you're typing. 
-function wordsPerMinute(toldString, minutes) {
+function wordsPerTime(toldString, time) {
      //\s means "one space", and \s+ means "one or more spaces".
-     if(minutes <= 0){
-        return "Please type for more than 1 minute.";
+     if(time <= 0){
+        return 0;
       } else{
       let regularExpression =/\s+/;
       let stringWords = toldString.trim().split(regularExpression);
-      return stringWords.length/minutes;}
+      
+      if(minutes == timer[0]){
+          return stringWords.length/time;
+        }else{
+            return stringWords.length/time;
+      }
   }//by Royer Adames
-
+}
 
 //  you can do something really advanced like add an array of different texts so the tester can switch between different texts to test or even 
-// add a high score board.
+function getRandomIndex(){
+    return Math.floor(Math.random() * Math.floor(arrayText.length));
+}
+
+// add a high score board
